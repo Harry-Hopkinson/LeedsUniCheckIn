401b4948b11c1d5d8018b49c62d9f60b5d29ac3c
Harry Hopkinson <harryjhopkinson@gmail.com>
2025-10-20 11:40:47 +0100

    Add data.txt to store example qrcode data

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,17 @@ fn get_exe_path() -> std::path::PathBuf {
         .unwrap()
         .to_path_buf();
 }
+
+fn load_txt_data() -> Vec<String> {
+    let file_path = get_exe_path().join("../../data/data.txt");
+    let file_contents = std::fs::read_to_string(file_path).unwrap();
+    file_contents.lines().map(|line| line.to_string()).collect()
+}
+
 fn main() {
     let code = QrCode::new(b"01234567").unwrap();
+
+    println!("{}", load_txt_data().join("\n"));
 
     let image = code.render::<Luma<u8>>().build();
     println!("{:?}", get_exe_path());
