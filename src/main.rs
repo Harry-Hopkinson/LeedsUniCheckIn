fa2cd43785d2d4c047e20588bbb82c5b8745904c
Harry Hopkinson <harryjhopkinson@gmail.com>
2025-10-20 11:36:13 +0100

    Create qrcode with arbitary data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use image::Luma;
+use qrcode::QrCode;
+
+fn get_exe_path() -> std::path::PathBuf {
+    return std::env::current_exe()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .to_path_buf();
+}
 fn main() {
-    println!("Hello, world!");
+    let code = QrCode::new(b"01234567").unwrap();
+
+    let image = code.render::<Luma<u8>>().build();
+    println!("{:?}", get_exe_path());
+
+    image.save(get_exe_path().join("qrcode.png")).unwrap();
 }
