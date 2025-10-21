038035b80249f34d1410df340181e68169fc344d
Harry Hopkinson <harryjhopkinson@gmail.com>
2025-10-21 18:53:59 +0100

    Scrape all rooms and load them from a text file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
+use std::path::PathBuf;
+
 use dialoguer::{FuzzySelect, theme::ColorfulTheme};
 use image::Luma;
 use owo_colors::OwoColorize;
 use qrcode::QrCode;
 
-mod path;
+fn load_rooms() -> Vec<&'static str> {
+    include_str!("rooms.txt").lines().collect()
+}
 
-const ROOMS: &[&str] = &[
-    "11-14 Blenheim Terrace SR (1.01)",
-    "11-14 Blenheim Terrace SR (1.17)",
-    "11-14 Blenheim Terrace SR (G.02)",
-    "11-14 Blenheim Terrace SR (G.03)",
-    "11-14 Blenheim Terrace SR (G.06)",
-    "11-14 Blenheim Terrace SR (G.08)",
-    "11-14 Blenheim Terrace SR (G.12)",
-];
-
-const DATA: &str = r#"{"locationName": "Worsley SR (8.49n)", "description": "Worsley SR (8.49n)"}"#;
+fn get_exe_path() -> PathBuf {
+    std::env::current_exe()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .to_path_buf()
+}
 
 fn main() {
     println!(
@@ -30,17 +30,22 @@ fn main() {
 
     let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
         .default(0)
-        .items(&ROOMS[..])
+        .items(&load_rooms())
         .interact()
         .unwrap();
 
-    let room = ROOMS[selection];
+    let room = load_rooms()[selection];
     println!("You have selected room: {}", room.blue().italic().bold());
 
-    let code = QrCode::new(DATA).expect("Failed to create QR code");
+    let data = format!(
+        r#"{{"locationName": "{}", "description": "{}"}}"#,
+        room, room
+    );
+
+    let code = QrCode::new(data).expect("Failed to create QR code");
     let image = code.render::<Luma<u8>>().build();
 
-    let output_path = path::get_exe_path().join("qrcode.png");
+    let output_path = get_exe_path().join("qrcode.png");
     image
         .save(&output_path)
         .expect("Failed to save QR code image");
--- src/path.rs
+++
@@ -1,9 +0,0 @@
-use std::path::PathBuf;
-
-pub fn get_exe_path() -> PathBuf {
-    return std::env::current_exe()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .to_path_buf();
-}
